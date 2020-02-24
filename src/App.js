9b5678ddc929a7bed754575fced83cbfde3c1e2a
Olegus <gulldan1996@gmail.com>
2020-02-24 19:39:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Spinner from "./Components/Spinner";
 
-function App() {
+const RepeatButton = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      aria-label="Play again."
+      id="repeatButton"
+      onClick={props.onClick}
+    ></button>
   );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null
+    };
+    this.finishHandler = this.finishHandler.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ winner: null });
+    this.emptyArray();
+    this._child1.forceUpdateHandler();
+    this._child2.forceUpdateHandler();
+    this._child3.forceUpdateHandler();
+    this._child4.forceUpdateHandler();
+    this._child5.forceUpdateHandler();
+  }
+
+  static matches = [];
+
+  finishHandler(value) {
+    App.matches.push(value);
+
+    if (App.matches.length === 5) {
+      const first = App.matches[0];
+      let results = App.matches.every(match => match === first);
+      this.setState({ winner: results });
+    }
+  }
+
+  emptyArray() {
+    App.matches = [];
+  }
+
+
+  render() {
+    const { winner } = this.state;
+    let repeatButton = null;
+
+    if (winner !== null) {
+      repeatButton = <RepeatButton onClick={this.handleClick} />;
+    }
+
+    return (
+      <div>
+        <div className={`spinner-container`}>
+          <Spinner
+            onFinish={this.finishHandler}
+            ref={child => {
+              this._child1 = child;
+            }}
+            timer="1000"
+          />
+          <Spinner
+            onFinish={this.finishHandler}
+            ref={child => {
+              this._child2 = child;
+            }}
+            timer="1200"
+          />
+          <Spinner
+            onFinish={this.finishHandler}
+            ref={child => {
+              this._child3 = child;
+            }}
+            timer="1400"
+          />
+          <Spinner
+            onFinish={this.finishHandler}
+            ref={child => {
+              this._child4 = child;
+            }}
+            timer="1800"
+          />
+          <Spinner
+            onFinish={this.finishHandler}
+            ref={child => {
+              this._child5 = child;
+            }}
+            timer="2000"
+          />
+          <div className="gradient-fade"></div>
+        </div>
+        {repeatButton}
+      </div>
+    );
+  }
 }
 
 export default App;
