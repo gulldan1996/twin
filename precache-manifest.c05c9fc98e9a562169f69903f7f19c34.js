4b2d8d205bed1d387f622a09b3afa7b4ec549917
Olegus <gulldan1996@gmail.com>
2020-02-24 19:59:27 +0200

    Updates

---
+++ precache-manifest.c05c9fc98e9a562169f69903f7f19c34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54618093b97910acf8e22febf5aa59b5",
+    "url": "/twin/index.html"
+  },
+  {
+    "revision": "0d228ec421f8d857f408",
+    "url": "/twin/static/css/main.bd737e3f.chunk.css"
+  },
+  {
+    "revision": "7b02b0437c9b584fb5f4",
+    "url": "/twin/static/js/2.7a62da25.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/twin/static/js/2.7a62da25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d228ec421f8d857f408",
+    "url": "/twin/static/js/main.93dfef5d.chunk.js"
+  },
+  {
+    "revision": "27ad742261c68d7859f9",
+    "url": "/twin/static/js/runtime-main.2be96e88.js"
+  },
+  {
+    "revision": "b9fef22d09838279b4b31f26d397776c",
+    "url": "/twin/static/media/css_sprites.b9fef22d.png"
+  }
+]);
